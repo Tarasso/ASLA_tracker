2a25039298123bd6a949167e62ac403341794b69
piercefu <fu.pierce123@gmail.com>
2022-02-12 15:23:38 -0600

    added home page as view with a new controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  #get 'pages/home'
+  get 'pages/about'
+  get 'pages/contact'
   resources :student_members
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'pages#home'
 end
