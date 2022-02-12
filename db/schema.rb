6c957dcdc0a9e828099222ce38559536ca31be55
Subrajit <subr809@tamu.edu>
2022-02-12 15:02:36 -0600

    add schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_02_11_165216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.date "date"
+    t.string "name"
+    t.string "location"
+    t.time "start_time"
+    t.string "description"
+    t.integer "event_type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.time "finish_time"
+  end
 
 end
