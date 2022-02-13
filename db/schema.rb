4dc718ccda9bd0c781bacd857c9f58fe3aaf7e34
Michael Cadieux <mcadieux2023@tamu.edu>
2022-02-13 10:43:31 -0600

    finished calendar, added nav bar to each page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_12_210944) do
+ActiveRecord::Schema.define(version: 2022_02_12_223054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 2022_02_12_210944) do
     t.string "last_name"
     t.string "phone_num"
     t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "calendars", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
