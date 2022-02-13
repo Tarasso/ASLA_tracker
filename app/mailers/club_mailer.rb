09dae638715d644bb6ce9430d09b5c60f67e859a
root <root@localhost>
2022-02-13 21:13:49 +0000

    fixed rubocop errors

--- app/mailers/club_mailer.rb
+++ app/mailers/club_mailer.rb
@@ -1,31 +1,26 @@
+# frozen_string_literal: true
+
 class ClubMailer < ApplicationMailer
-    default from: "testemailfor431@gmail.com"
+  default from: 'testemailfor431@gmail.com'
 
-    layout "mailer"
+  layout 'mailer'
 
-    def send_mail
-        @subject = params[:subject]
-        @emails = params[:emails]
-        @body = params[:body]
-        @group1 = params[:group1]
-        @group2 = params[:group2]
-        @group3 = params[:group3]
+  def send_mail
+    @subject = params[:subject]
+    @emails = params[:emails]
+    @body = params[:body]
+    @group1 = params[:group1]
+    @group2 = params[:group2]
+    @group3 = params[:group3]
 
+    recipients = @emails.split(', ')
 
-        recipients = @emails.split(", ")
+    recipients += StudentMember.where(member_title: '1').pluck(:email) if @group1 == 'officers'
 
-        if @group1 == "officers"
-            recipients += StudentMember.where(member_title: '1').pluck(:email)
-        end
+    recipients += StudentMember.where(member_title: '0').pluck(:email) if @group2 == 'non_officers'
 
-        if @group2 == "non_officers"
-            recipients += StudentMember.where(member_title: '0').pluck(:email)
-        end
+    recipients += BusinessProfessional.pluck(:email) if @group3 == 'business'
 
-        if @group3 == "business"
-            recipients += BusinessProfessional.pluck(:email)
-        end
-
-        mail(to: recipients.uniq, subject: @subject)
-    end
+    mail(to: recipients.uniq, subject: @subject)
+  end
 end
