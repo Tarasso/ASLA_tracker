2c498bd98f254ffc9f3537a76b207653c8b0e8a7
piercefu <fu.pierce123@gmail.com>
2022-02-14 11:54:01 -0600

    added unit testing and more integratino testing

--- app/models/student_member.rb
+++ app/models/student_member.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class StudentMember < ApplicationRecord
-  enum member_title: { member: 0, officer: 1 }
+  enum member_title: { member: 1, officer: 2 }
+  validates :uin, :first_name, :last_name, :class_year, :member_title, :email, presence: true
 end
--- spec/student/feature/integration_spec.rb
+++ spec/student/feature/integration_spec.rb
@@ -65,4 +65,49 @@ RSpec.describe('Creating a Student Member', type: :feature) do
     expect(page).to(have_content('false'))
     expect(page).to(have_content('Hello'))
   end
+
+  #similar test, but different choices for drop down menus and testing for the show page
+  it 'valid inputs' do
+    visit new_student_member_path
+
+    fill_in 'Uin', with: '1336443725'
+    fill_in 'First name', with: 'Pierce'
+    fill_in 'Last name', with: 'Ju'
+    fill_in 'Class year', with: '2023'
+    select '2017', from: 'student_member_join_date_1i'
+    select 'May', from: 'student_member_join_date_2i'
+    select '5', from: 'student_member_join_date_3i'
+    select 'Officer', from: 'student_member_member_title'
+    fill_in 'Email', with: 'fu.pierce123@gmail.edu'
+    fill_in 'Phone number', with: '13991328204'
+    select '2025', from: 'student_member_expected_graduation_date_1i'
+    select 'May', from: 'student_member_expected_graduation_date_2i'
+    select '6', from: 'student_member_expected_graduation_date_3i'
+    fill_in 'Social point amount', with: 999
+    fill_in 'Meeting point amount', with: 998
+    fill_in 'Fundraiser point amount', with: 997
+    fill_in 'Informational point amount', with: -1
+    fill_in 'Officer title', with: 'President'
+    select 'Yes', from: 'student_member_dues_paid'
+    fill_in 'Picture', with: 'Handsome photo'
+    click_on 'Create Student member'
+
+    expect(page).to(have_content('1336443725'))
+    expect(page).to(have_content('Pierce'))
+    expect(page).to(have_content('Ju'))
+    expect(page).to(have_content('2023'))
+    expect(page).to(have_content('2017-05-05'))
+    expect(page).to(have_content('officer'))
+    expect(page).to(have_content('fu.pierce123@gmail.edu'))
+    expect(page).to(have_content('13991328204'))
+    expect(page).to(have_content('2025-05-06'))
+    expect(page).to(have_content('999'))
+    expect(page).to(have_content('998'))
+    expect(page).to(have_content('997'))
+    expect(page).to(have_content('-1'))
+    expect(page).to(have_content('President'))
+    expect(page).to(have_content('true'))
+    expect(page).to(have_content('Handsome photo'))
+  end
+  
 end
