1880f3087afb0be409c5e5791c10cc7cf8744c82
root <root@localhost>
2022-02-22 22:43:38 +0000

    added auth back on pages and fixed existing tests

--- spec/feature/login_integration_spec.rb
+++ spec/feature/login_integration_spec.rb
@@ -3,16 +3,16 @@
 # location: spec/feature/integration_spec.rb
 require 'rails_helper'
 
-RSpec.describe('Creating a Student Member', type: :feature) do
+RSpec.describe('Loggin into an existing account', type: :feature) do
   before do
     Rails.application.env_config['devise.mapping'] = Devise.mappings[:user]
     Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google_user]
-    visit root_path
-    click_on 'Sign in'
   end
 
-  it 'valid access' do
-    visit calendars_path
-    expect(page).to(have_content('Google Calendar'))
+  it 'signs in' do
+    visit root_path
+    click_on 'Sign in'
+
+    expect(page).to(have_content('Successfully authenticated from Google account.'))
   end
 end
--- spec/student/feature/integration_spec.rb
+++ spec/student/feature/integration_spec.rb
@@ -13,6 +13,8 @@ RSpec.describe('Creating a Student Member', type: :feature) do
   before do
     Rails.application.env_config['devise.mapping'] = Devise.mappings[:user]
     Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google_user]
+    visit root_path
+    click_on 'Sign in'
     # Permission.create!(description: 'admin') if Permission.where(description: 'admin').first.nil?
     # unless Admin.where(email: 'admindoe@example.com').first.nil? == false
     #  Admin.create!(email: 'admindoe@example.com', full_name: 'Admin Doe', uid: '234567890', avatar_url: 'https://lh3.googleusercontent.com/url/photo.jpg')
