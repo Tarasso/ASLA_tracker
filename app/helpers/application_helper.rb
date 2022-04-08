b1f481fa87891bf1707399dfec3c1cbdd7c0b81f
Pranav Jain <pranavji@tamu.edu>
2022-04-08 17:27:30 -0500

    time changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,13 @@ module ApplicationHelper
   def read_bool(boolean)
     boolean ? 'Yes' : 'No'
   end
+
+
+  def daylight_savings(dt)
+    if Time.zone.now.dst?
+      return dt + 1.hour 
+    else
+      return dt
+    end  
+  end
 end
