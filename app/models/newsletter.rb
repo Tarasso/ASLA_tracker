faf2dfe37a7eae7583ed91deabbfd4c2dc96943f
Pranav Jain <pranavji@tamu.edu>
2022-04-08 13:40:05 -0500

    fixed time stuff

--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Newsletter < ApplicationRecord
-  validates :date_posted, presence: true
   validates :message, presence: true
   validates :title, presence: true
   has_many_attached :images
--- db/migrate/20220212210944_create_newsletters.rb
+++ db/migrate/20220212210944_create_newsletters.rb
@@ -1,7 +1,6 @@
 class CreateNewsletters < ActiveRecord::Migration[6.1]
   def change
     create_table :newsletters do |t|
-      t.date :date_posted
       t.string :message
       t.string :title
 
